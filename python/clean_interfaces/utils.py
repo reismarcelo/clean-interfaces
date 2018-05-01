025d967deaea901c643e209f72bcc25abb140265
Marcelo Reis <mareis@cisco.com>
2018-05-01 09:31:32 -0400

    Updated apply_template

--- python/clean_interfaces/utils.py
+++ python/clean_interfaces/utils.py
@@ -57,20 +57,24 @@ def init_plan(plan_component, *custom_states):
     return plan_component
 
 
-def apply_template(template_name, context, var_dict=None):
+def apply_template(template_name, context, var_dict=None, none_value=''):
     """
     Facilitate applying templates by setting template variables via an optional dictionary
+
+    By default, if a dictionary item has value None it is converted to an empty string. In the template,
+    'when' statements can be used to prevent rendering of a template block if the variable is an empty string.
 
     :param template_name: Name of the template file
     :param context: Context in which the template is rendered
     :param var_dict: Optional dictionary containing additional variables to be passed to the template
+    :param none_value: Optional, defines the replacement for variables with None values. Default is an empty string.
     """
     template = ncs.template.Template(context)
     t_vars = ncs.template.Variables()
 
     if var_dict is not None:
         for name, value in var_dict.items():
-            t_vars.add(name, value)
+            t_vars.add(name, value or none_value)
 
     template.apply(template_name, t_vars)
 
